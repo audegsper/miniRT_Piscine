d69856442a35e2fa0a3fdb8c73d0c1568431a8eb
DoH <dohykim@DoHui-MacBookPro.local>
2021-10-16 00:14:14 +0900

    15th complete

--- srcs/parse/get/ambient.c
+++ srcs/parse/get/ambient.c
@@ -6,7 +6,7 @@
 /*   By: dohykim <dohykim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 21:59:59 by hyson             #+#    #+#             */
-/*   Updated: 2021/10/15 23:35:44 by dohykim          ###   ########.fr       */
+/*   Updated: 2021/10/16 00:09:58 by dohykim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,7 @@ t_bool	get_ambient(t_object_condition *ob, char *line)
 	ob = 0;
 	char *tmp = line;
 	tmp = line;
+
+	//a 옵션들 확인해주는 모듈필요
+	return (TRUE); // 일단 아무거나 넣었음. 옵션들 범위 이상있을 경우 false 반환
 }
