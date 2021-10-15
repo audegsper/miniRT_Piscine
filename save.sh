ef79cd7f6d6a1781d5e3f5876beb0f9bab8b3808
DoH <dohykim@DoHui-MacBookPro.local>
2021-10-15 19:57:37 +0900

    test confirm

--- save.sh
+++ save.sh
@@ -1,4 +1,6 @@
 cp -r * ../backup/
 git add *
-git commit -m "save"
+echo -e "git commit -m에 넣을 메세지를 넣어주세요."
+read  msg
+git commit -m "$msg"
 git push https://github.com/audegsper/miniRT_Piscine.git
